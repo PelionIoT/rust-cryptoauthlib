61c7e23d8c0617ba8bbe8e8d5257cf4a7fa720ec
Robert Drazkowski <Robert.Drazkowski@globallogic.com>
2020-10-30 14:07:16 +0100

    Compilation improvements

--- cryptoauthlib-sys/build.rs
+++ cryptoauthlib-sys/build.rs
@@ -3,7 +3,7 @@ use cmake;
 fn main()
 {
     let dst = cmake::Config::new("cryptoauthlib")
-                     .build_target("") // Prevent installing in host system directories
+                     .no_build_target(true) // Prevent installing in host system directories
                      .define("ATCA_HAL_I2C", "ON")
                      .build();       
     // Below caller LD_FLAGS are defined. First -L then -l
