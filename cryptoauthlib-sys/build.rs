295d389747bab3fa2f347bbb1a06a4241812d75b
Robert Drazkowski <Robert.Drazkowski@globallogic.com>
2020-12-08 11:11:12 +0100

    Added "rpath" settings to build.rs. Removed useless tests.

--- cryptoauthlib-sys/build.rs
+++ cryptoauthlib-sys/build.rs
@@ -7,6 +7,8 @@ fn main()
                      .define("ATCA_HAL_I2C", "ON")
                      .build();       
     // Below caller LD_FLAGS are defined. First -L then -l
-    println!("cargo:rustc-link-search=native={}/build/lib", dst.display());
-    println!("cargo:rustc-link-lib=dylib=cryptoauth");    
+    println!("cargo:rustc-link-search=all={}/build/lib", dst.display());
+    println!("cargo:rustc-link-lib=dylib=cryptoauth");
+    
+    println!("cargo:rustc-cdylib-link-arg=-Wl,-rpath,{}/build/lib",dst.display())
 }
--- cryptoauthlib-sys/src/lib.rs
+++ cryptoauthlib-sys/src/lib.rs
@@ -3,11 +3,3 @@
 #![allow(non_snake_case)]
 
 include!("./bindings.rs");
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
