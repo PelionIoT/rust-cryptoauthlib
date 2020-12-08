98b01d108da08cfd5ec49a98c68930be2a08ab0a
Robert Drazkowski <Robert.Drazkowski@globallogic.com>
2020-12-08 11:45:40 +0100

    Switched to building a static CA library.

--- cryptoauthlib-sys/build.rs
+++ cryptoauthlib-sys/build.rs
@@ -4,11 +4,10 @@ fn main()
 {
     let dst = cmake::Config::new("cryptoauthlib")
                      .no_build_target(true) // Prevent installing in host system directories
-                     .define("ATCA_HAL_I2C", "ON")
+                     .define("ATCA_BUILD_SHARED_LIBS", "0") // Build CAL as a static library
+                     .define("ATCA_HAL_I2C", "ON") // Include i2c support
                      .build();       
     // Below caller LD_FLAGS are defined. First -L then -l
-    println!("cargo:rustc-link-search=all={}/build/lib", dst.display());
-    println!("cargo:rustc-link-lib=dylib=cryptoauth");
-    
-    println!("cargo:rustc-cdylib-link-arg=-Wl,-rpath,{}/build/lib",dst.display())
+    println!("cargo:rustc-link-search=native={}/build/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=cryptoauth");
 }
